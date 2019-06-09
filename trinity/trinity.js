4a25a43072b79657b4d51dcadfef742cc255f0fd
Rainner Lins <rainnerlins@gmail.com>
2019-06-08 22:17:02 -0400

    Added trinity college login.

---
+++ trinity/trinity.js
@@ -0,0 +1,59 @@
+/**
+ * login form scripts.
+ */
+(function() {
+  const $form      = $( '.trinity-form' );
+  const $welcome   = $( '.trinity-welcome', $form );
+  const $email     = $( '.trinity-email', $form );
+  const $password  = $( '.trinity-password', $form );
+  if ( !$form.length ) return;
+
+  // switch form state when email is present
+  const switchState = function( email='' ) {
+    if ( email ) {
+      $( 'span', $welcome ).text( email );
+      $welcome.addClass( 'visible' );
+      $email.addClass( 'hidden' );
+      $password.removeClass( 'hidden' );
+    } else {
+      $form.trigger( 'reset' );
+      $( 'input', $form ).trigger( 'blur' );
+      $( 'span', $welcome ).empty();
+      $welcome.removeClass( 'visible' );
+      $email.removeClass( 'hidden' );
+      $password.addClass( 'hidden' );
+    }
+  }
+
+  // reset form when selected email is clicked
+  $( 'button', $welcome ).on( 'click', function() {
+    switchState();
+  });
+
+  // ignore form submit until later
+  $form.on( 'submit', function( e ) {
+    e.preventDefault();
+    const _email = String( this._email.value || '' ).trim();
+    const _password = String( this._password.value || '' ).trim();
+    switchState( _email );
+
+    // check for email
+    if ( !_email ) {
+      this._email.focus();
+      return false;
+    }
+    // check for password
+    if ( !_password ) {
+      this._password.focus();
+      return false;
+    }
+    // form ready to be processed
+    console.log( _email, _password );
+    window.location.assign( this.action || '#' );
+    return true;
+  });
+
+  // reset inputs
+  switchState();
+
+})();
